a9f301dae0be4eb1ef7b1f94fa3ec85813eae5cf
Naval Sood <naval.sood@tarkashilpa.com>
2023-04-25 13:12:56 +0530

    party schema corrected
    Signed-off-by: Naval Sood <naval.sood@tarkashilpa.com>

---
+++ src/api/party-profile/routes/party-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * party-profile router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::party-profile.party-profile');
---
+++ src/api/party-profile/services/party-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * party-profile service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::party-profile.party-profile');
