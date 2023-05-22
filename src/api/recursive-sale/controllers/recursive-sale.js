101e9e250b3452967455cf06791ff8931a3c2a78
Naval Sood <naval.sood@tarkashilpa.com>
2023-05-22 10:22:58 +0530

    Recursive sales logic

--- src/api/recursive-sale/controllers/recursive-sale.js
+++ src/api/recursive-sale/controllers/recursive-sale.js
@@ -1,4 +1,5 @@
 'use strict';
+const { parseMultipartData } = require("@strapi/utils");
 
 /**
  * recursive-sale controller
@@ -6,4 +7,51 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recursive-sale.recursive-sale');
+module.exports = createCoreController('api::recursive-sale.recursive-sale', ({ strapi }) => ({
+    async create(ctx) {
+        const { data } = parseMultipartData(ctx);
+        let recursiveSalesData = { ...data };
+        let recursiveSaleId;
+        let recursiveSale = await strapi.service("api::recursive-sale.recursive-sale").create(recursiveSalesData);
+        recursiveSaleId = recursiveSale.id;
+        let { 
+            RECURRING_FREQUENCY,
+            EXPECTED_SALE_COMPLETION_DATE,
+            MONTH_OF_SALE_DONE,
+            NUMBER_OF_RECURRENCES,
+            DESCRIPTION,
+            services,
+            AMOUNT_WITHOUT_TAXES,
+            SALES_REF,
+            PROJECT
+         } = recursiveSalesData
+
+        let salesPayload = {
+            PROJECT: PROJECT,
+            STATUS_OF_PROJECT: "UPCOMING",
+            EXPECTED_COMPLETION_DATE: EXPECTED_SALE_COMPLETION_DATE,
+            MONTH_OF_SALE: MONTH_OF_SALE_DONE,
+            DESCRIPTION: DESCRIPTION,
+            AMOUNT: AMOUNT_WITHOUT_TAXES,
+            services: services,
+            recursive_sales: [recursiveSaleId],
+        };
+        let salesArr = []
+        for (let index = 0; index < (RECURRING_FREQUENCY-1) ; index++) {
+            salesArr.push(salesPayload)
+        }
+        let results = await strapi.service("api::sale.sale").createMany({ data: salesArr });
+        let salesIds = results.ids
+        let tempSales = await strapi.service("api::recursive-sale.recursive-sale").update(recursiveSaleId,
+            {
+                data: {
+                    SALES_REF: salesIds
+                }
+            }
+        );
+
+        const sanitizedResults = await this.sanitizeOutput(tempSales, ctx);
+        return this.transformResponse(sanitizedResults);
+    },
+})
+)
--- src/api/sale/services/sale.js
+++ src/api/sale/services/sale.js
@@ -7,3 +7,9 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::sale.sale');
+module.exports = {
+    createMany: async function (data) {
+      const results = await strapi.query("api::sale.sale").createMany(data);
+      return results;
+    },
+  };
