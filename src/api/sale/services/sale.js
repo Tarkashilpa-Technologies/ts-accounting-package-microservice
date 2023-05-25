4c2db592e359d15e58f7de40b3f2129df56ee885
Naval Sood <naval.sood@tarkashilpa.com>
2023-05-25 10:44:21 +0530

    Update sale.js

--- src/api/sale/services/sale.js
+++ src/api/sale/services/sale.js
@@ -6,10 +6,15 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::sale.sale');
-module.exports = {
-    createMany: async function (data) {
-      const results = await strapi.query("api::sale.sale").createMany(data);
-      return results;
-    },
-  };
+module.exports = createCoreService('api::sale.sale'), ({ strapi }) =>  ({
+  async createMany (data) {
+    const results = await strapi.query("api::sale.sale").createMany(data);
+    return results;
+  },
+})
+// module.exports = {
+//     createMany: async function (data) {
+//       const results = await strapi.query("api::sale.sale").createMany(data);
+//       return results;
+//     },
+//   };
