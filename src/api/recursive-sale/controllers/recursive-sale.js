d1aff62783287f96cca6961c27d14debbe8ecf99
Naval Sood <naval.sood@tarkashilpa.com>
2023-05-26 01:39:06 +0530

    Recursive sale logic corrections

--- src/api/recursive-sale/controllers/recursive-sale.js
+++ src/api/recursive-sale/controllers/recursive-sale.js
@@ -11,47 +11,98 @@ module.exports = createCoreController('api::recursive-sale.recursive-sale', ({ s
     async create(ctx) {
         const { data } = parseMultipartData(ctx);
         let recursiveSalesData = { ...data };
-        let recursiveSaleId;
         let recursiveSale = await strapi.service("api::recursive-sale.recursive-sale").create(recursiveSalesData);
-        recursiveSaleId = recursiveSale.id;
-        let { 
-            RECURRING_FREQUENCY,
-            EXPECTED_SALE_COMPLETION_DATE,
-            MONTH_OF_SALE_DONE,
-            NUMBER_OF_RECURRENCES,
-            DESCRIPTION,
-            services,
-            AMOUNT_WITHOUT_TAXES,
-            SALES_REF,
-            PROJECT
-         } = recursiveSalesData
-
+        let recursiveSaleId = recursiveSale.id;
+        console.log(recursiveSalesData)
         let salesPayload = {
-            PROJECT: PROJECT,
+            PROJECT: recursiveSalesData?.data?.PROJECT,
             STATUS_OF_PROJECT: "UPCOMING",
-            EXPECTED_COMPLETION_DATE: EXPECTED_SALE_COMPLETION_DATE,
-            MONTH_OF_SALE: MONTH_OF_SALE_DONE,
-            DESCRIPTION: DESCRIPTION,
-            AMOUNT: AMOUNT_WITHOUT_TAXES,
-            services: services,
-            recursive_sales: [recursiveSaleId],
+            EXPECTED_COMPLETION_DATE: "2023-05-09",
+            MONTH_OF_SALE: "2023-05-09",
+            DESCRIPTION: "this is a local project",
+            AMOUNT: 0,
+            services: recursiveSalesData?.data?.services,
+            recursive_sale: recursiveSaleId,
         };
-        let salesArr = []
-        for (let index = 0; index < (RECURRING_FREQUENCY-1) ; index++) {
-            salesArr.push(salesPayload)
+        let salesIds = [];
+        for (
+            let index = 0;
+            index < recursiveSalesData.data.NUMBER_OF_RECURRENCES;
+            index++
+        ) {
+            let result = await strapi.db
+                .query("api::sale.sale")
+                .create({ data: salesPayload });
+            salesIds.push(result.id);
         }
-        let results = await strapi.service("api::sale.sale").createMany({ data: salesArr });
-        let salesIds = results.ids
-        let tempSales = await strapi.service("api::recursive-sale.recursive-sale").update(recursiveSaleId,
-            {
+        console.log("ABCD", salesIds, recursiveSaleId, )
+        recursiveSalesData.data.sales = salesIds;
+        console.log(recursiveSalesData)
+        let tempSales = await strapi
+            .service("api::recursive-sale.recursive-sale")
+            .update(recursiveSaleId, {
                 data: {
-                    SALES_REF: salesIds
-                }
-            }
-        );
-
+                    sales: salesIds,
+                },
+            });
         const sanitizedResults = await this.sanitizeOutput(tempSales, ctx);
         return this.transformResponse(sanitizedResults);
     },
+
+    async update(ctx) {
+        const { data } = parseMultipartData(ctx);
+        let recursiveSaleId = ctx.params.id;
+        let recursiveSalesData = { ...data };
+        let recursiveSale = await strapi
+            .service("api::recursive-sale.recursive-sale")
+            .update(recursiveSaleId, recursiveSalesData);
+        let salesPayload = {
+            PROJECT: "test 1234",
+            STATUS_OF_PROJECT: "UPCOMING",
+            EXPECTED_COMPLETION_DATE: "2023-05-09",
+            MONTH_OF_SALE: "2023-05-09",
+            DESCRIPTION: "this is a local project abc",
+            AMOUNT: 0,
+            services: recursiveSalesData.data.services,
+        };
+
+        const SalesToUpdate = await strapi.db.query("api::sale.sale").findMany({
+            where: {
+                recursive_sales: [15],
+              }
+            }
+          );
+          await Promise.all(
+            SalesToUpdate.map(({ id }) =>{
+                strapi.service("api::sale.sale").update(id, recursiveSalesData)
+            }
+            )
+          );
+        const sanitizedResults = await this.sanitizeOutput(recursiveSale, ctx);
+        return this.transformResponse(sanitizedResults);
+    },
+
+    // async delete(ctx) {
+    //     const { data } = parseMultipartData(ctx);
+    //     let recursiveSaleId = ctx.params.id;
+    //     let recursiveSalesData = { ...data };
+
+    //     const SalesToUpdate = await strapi.db.query("api::sale.sale").findMany({
+    //         where: {
+    //             recursive_sales: [15],
+    //         }
+    //     }
+    //     );
+    //     await Promise.all(
+    //         SalesToUpdate.map(({ id }) => {
+    //             strapi.db.query("api::lamp.lamp").delete({
+    //                 where: { id },
+    //             })
+    //         }
+    //         )
+    //     );
+    //     const sanitizedResults = await this.sanitizeOutput(recursiveSale, ctx);
+    //     return this.transformResponse(sanitizedResults);
+    // },
 })
 )
